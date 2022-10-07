70a2154cb5a1395a5e616c04a1e9850c605bf9d8
rahul <neorahul@outlook.in>
2022-10-07 19:10:55 +0530

    initial commited

---
+++ RandomNumberG.py
@@ -0,0 +1,21 @@
+import random
+randomNum=random.randint(1,100)
+
+# print(randomNum)
+userinput=None
+guess=0
+
+while randomNum!=userinput:
+    userinput=int(input("Guess any number ! \n"))
+    guess+=1
+    if randomNum==userinput:
+        print("hye ! you guessed correct number")
+    else:
+        if userinput>randomNum:
+            print("Opps ! you guessed too high ")
+        else:
+            print("Opps ! you guessed too low ")
+
+print(f"wow! you guessed the number only in {guess} times ")
+
+print("All most completed.\nNeeded file handling ?")
